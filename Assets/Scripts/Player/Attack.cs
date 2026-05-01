cb388ca322ad9fdc50c87d20c3dd4d1d0ce259e7
AgusCeb <agustincblls@gmail.com>
2026-04-30 21:27:44 -0300

    terrain parte 2 y equipo de espada

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     [SerializeField] private GameObject _sword;
+    [SerializeField] private GameObject _backSword;
     [SerializeField] private float _attackColdown = 0.3f;
 
     private void Start()
     {
         _sword.SetActive(false);
+        _backSword.SetActive(false);
+    }
+
+    private void OnEquip()
+    {
+        _backSword.SetActive(!_backSword.activeSelf);
     }
 
     private void OnAttack()
