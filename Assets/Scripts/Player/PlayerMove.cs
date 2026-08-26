d003136480f49ba1541ebee563474073403e1945
Agus <agustincblls@gmail.com>
2026-08-26 17:29:19 -0300

    Cambios A La Gravedad del Personaje En Rampas y en general

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMove : NetworkBehaviour
 {
@@ -38,6 +40,14 @@ public class PlayerMove : NetworkBehaviour
     [SerializeField] private float _dashCooldown;
     [SerializeField] private AudioClip _dashSFX;
     private bool _canDash = true;
+
+    //Rampas
+    [Header("Angulo para Rampas")]
+    [SerializeField] private float _maxAngle;
+    [SerializeField] private float _rampCheckDistance;
+    [SerializeField] private float _downForce;
+    private bool _isFalling;
+    private bool _isJumping;
 
     private void Start()
     {
@@ -78,9 +88,12 @@ public class PlayerMove : NetworkBehaviour
         if (Value.isPressed && _isGrounded)
         {
             _jump = true;
+            _isFalling = false;
         }
         
     }
+
+
     private Vector3 GetMoveDirection()
     {
         //movimento
@@ -96,13 +109,80 @@ public class PlayerMove : NetworkBehaviour
          return forward * _moveInput.y + right * _moveInput.x;
     }
 
+    private bool isOnRamp(out RaycastHit hit)
+    {
+        if (Physics.Raycast(_groundCheck.position, Vector3.down, out hit, _groundDistance + 0.2f, _groundLayer))
+        {
+            float angle = Vector3.Angle(Vector3.up, hit.normal);
+
+
+            Debug.DrawRay(
+                _groundCheck.position,
+                Vector3.down * _rampCheckDistance,
+                Color.red
+            );
+
+            Debug.DrawRay(
+                hit.point,
+                hit.normal,
+                Color.blue
+            );
+
+
+            return angle > 0f && angle <= _maxAngle;
+        
+        }
+
+        return false;
+    }
+
+
+    private Vector3 GetRampDirection(Vector3 direction)
+    {
+        direction.y=0f;
+        direction.Normalize();
+
+        Debug.DrawRay(
+        transform.position,
+        direction * _rampCheckDistance,
+        Color.red);
+
+        if (Physics.Raycast(_groundCheck.position, direction, out RaycastHit hit, _rampCheckDistance, _groundLayer))
+        {
+            float angle = Vector3.Angle(Vector3.up, hit.normal);
+            Debug.DrawRay(hit.point, hit.normal, Color.blue);
+
+            if(angle > 0f && angle<= _maxAngle)
+            {
+                Vector3 rampDirection = Vector3.ProjectOnPlane(direction, hit.normal).normalized;
+
+                Debug.DrawRay(
+                hit.point,
+                hit.normal,
+                Color.green);
+
+                return rampDirection;
+
+            }
+
+        }
+
+        return direction;
+
+    }
+
     
+
+
     private void FixedUpdate()
     {
         if (!IsOwner) return;
 
        
-        Vector3 moveDir = GetMoveDirection();
+        Vector3 moveDir = GetRampDirection(GetMoveDirection());
+
+        //comprueba si esta en una rampa
+        bool onRamp = isOnRamp(out RaycastHit rampHit);
         
         Vector3 targetVelocity = new Vector3(moveDir.x * _speed, Rbd.linearVelocity.y, moveDir.z * _speed);
         Rbd.linearVelocity = Vector3.Lerp(Rbd.linearVelocity,targetVelocity,0.2f);
@@ -113,9 +193,13 @@ public class PlayerMove : NetworkBehaviour
         //salto
         if (_isGrounded && _jump) 
         { 
+            Rbd.useGravity = true;
+
             Rbd.linearVelocity = new Vector3 (Rbd.linearVelocity.x,0f,Rbd.linearVelocity.z);
             Rbd.AddForce(Vector3.up*_jumpForce,ForceMode.Impulse);
             _jump = false;
+            _isJumping = true;
+            
         }
 
         //Rotacion de personaje
@@ -125,6 +209,30 @@ public class PlayerMove : NetworkBehaviour
 
             Rbd.MoveRotation(Quaternion.Slerp(Rbd.rotation, rotation, _rotationSpeed * Time.fixedDeltaTime));
         }
+
+        //Gravedad En Rampas
+        if (onRamp && !_jump)
+        {
+            Rbd.useGravity = false;
+
+            Rbd.AddForce(-rampHit.normal * _downForce,ForceMode.Force);
+        }
+        else
+        {
+            Rbd.useGravity = true;
+        }
+
+        if(_isJumping && Rbd.linearVelocity.y <= 0)
+        {
+            _isJumping = false;
+            _isFalling = true;
+        }
+
+        if (_isFalling)
+        {
+            Rbd.AddForce(Vector3.down * _downForce,ForceMode.Acceleration);
+        }
+
     }
     private IEnumerator Dash()
     {
@@ -137,6 +245,10 @@ public class PlayerMove : NetworkBehaviour
         {
             dashDir = transform.forward;
         }
+
+        dashDir = GetRampDirection(dashDir);
+        Rbd.linearVelocity = new Vector3(Rbd.linearVelocity.x, 0f, Rbd.linearVelocity.z);
+
         SFXManager.instance.PlaySFX(_dashSFX, this.transform, 1, 1);
         Rbd.AddForce(dashDir.normalized * _dashForce,ForceMode.Impulse);
         yield return new WaitForSeconds(_dashCooldown);
